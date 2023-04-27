383b5b09b58178d057f0049315172e52ad0279aa
Marcos <jmarcoshenrich@gmail.com>
2023-04-27 11:17:34 -0700

    version 1 complete

--- spellcheck/typo_algo.rb
+++ spellcheck/typo_algo.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 def likely_typo_obj_maker
     raw_typo_data = File.open("typo_data.txt")
     file_data = raw_typo_data.readlines.map(&:chomp)
@@ -12,21 +14,35 @@ def likely_typo_obj_maker
 end
 
 
-def queue_maker(base_word, char, i, num_inclusions, queue, training_data, likely_typo_obj, mod_string)
-    return if i > mod_string.length || num_inclusions == 0
+def inclusion_set_compiler(base_word, char, i, num_inclusions, set, training_data, likely_typo_obj, mod_string)
+    
+    return if !char || num_inclusions == 0
 
-    queue.push(base_word[0...i] + base_word[i] + base_word[i..-1]) # base letter is duplicated
+    dupword = mod_string[0...i] + mod_string[i] + mod_string[i..-1]
+    set.add(dupword) # base letter is duplicated
+    inclusion_set_compiler(base_word, base_word[i + 1], i + 1, num_inclusions, set, training_data, likely_typo_obj, dupword)
+
 
     likely_typo_obj[char].each_char do |typo_letter|
-        queue.push(base_word[0...i] + typo_letter + base_word[i..-1]) #extra letter is to the left of the base letter
-        queue.push(base_word[0..i] + typo_letter + base_word[i + 1..-1]) #extra letter is to the right of the base letter
+        leftword = mod_string[0..i] + typo_letter + mod_string[i + 1..-1]
+        rightword = mod_string[0...i] + typo_letter + mod_string[i..-1]
+        set.add(leftword) #extra letter is to the left of the base letter
+        set.add(rightword) #extra letter is to the right of the base letter
+        inclusion_set_compiler(base_word, base_word[i + 2], i + 2, num_inclusions - 1, set, training_data, likely_typo_obj, leftword)
+        inclusion_set_compiler(base_word, base_word[i + 2], i + 2, num_inclusions - 1, set, training_data, likely_typo_obj, rightword)
     end
 
-    queue.each do |mod_string|
-        queue_maker(base_word, base_word[i + 2], i + 2, num_inclusions - 1, training_data, mod_string)
-    end
+end
 
-    queue_maker(base_word, base_word[i + 1], i + 1, num_inclusions, training_data, mod_string)
+def letter_excluder(base_word, mod_string, index, level, training_data) 
+    return if level == 0 || index > mod_string.length - 1
+    sliced_string = mod_string.dup
+    sliced_string.slice!(index)
+    training_data[sliced_string] = base_word
+    training_data[mod_string] = base_word
+
+    letter_excluder(base_word, sliced_string, index, level - 1, training_data)
+    letter_excluder(base_word, mod_string, index + 1, level, training_data)
 end
 
 
@@ -43,65 +59,23 @@ def typo_setter(base_word, num_exclusions, num_inclusions, training_data)
 
     "question"
 
-    queue = []
+    inclusion_string_set = Set.new()
 
     i = 0
     while i < base_word.length
         char = base_word[i]
-
-        queue_maker(base_word, char, i, num_inclusions, queue, training_data, likely_typo_obj, base_word)
-
-        #make a function that 
-
-        # queue.push(base_word[0...i] + base_word[i] + base_word[i..-1]) # base letter is duplicated
-
-        # likely_typo_obj[char].each_char do |typo_letter|
-        #     queue.push(base_word[0...i] + typo_letter + base_word[i..-1]) #extra letter is to the left of the base letter
-        #     queue.push(base_word[0..i] + typo_letter + base_word[i + 1..-1]) #extra letter is to the right of the base letter
-        # end
-
-
-        # while queue.length > 0
-
-        # end
-        # outer loop iterates through each character
-        # inner loop through the likely typo arr and produce the following variations
-            # base letter is duplicated
-            # extra letter is to the left of the base letter
-            # extra letter is to the right of the base letter
-        # each variation gets run through letter_excluder
-
-        # each variation also needs to go through the inner loop again to account for multiple typos, controlled by num_inclusions
-
-        "arnold"
-
-        "aarnold" #
-            
-
+        inclusion_set_compiler(base_word, char, i, num_inclusions, inclusion_string_set, training_data, likely_typo_obj, base_word)
         i += 1
     end
 
-    p queue
+    inclusion_string_set.each { |mod_string| letter_excluder(base_word, mod_string, 0, num_exclusions, training_data)}
 
-    # letter_excluder(base_word, mod_string, 0, num_exclusions, training_data)
-
-    # training_data
+    training_data
 end
 
 
-typo_setter("arnold", 3, 3, {})
+p typo_setter("arnold", 3, 3, {})
 
 
-
-def letter_excluder(base_word, mod_string, index, level, training_data) 
-    return if level == 0 || index > mod_string.length - 1
-    sliced_string = mod_string.dup
-    sliced_string.slice!(index)
-    training_data[sliced_string] = base_word
-    training_data[mod_string] = base_word
-
-    letter_excluder(base_word, sliced_string, index, level - 1, training_data)
-    letter_excluder(base_word, mod_string, index + 1, level, training_data)
-end
 
 
